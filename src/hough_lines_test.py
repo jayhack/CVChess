e4a96e466055ab7f646d1dc0b4b5f2a6998c69fe
Jay Hack <jhack@stanford.edu>
2014-03-05 22:49:18 -0800

    now correctly getting the lines that make the grid

--- src/hough_lines_test.py
+++ src/hough_lines_test.py
@@ -65,6 +65,33 @@ def avg_close_lines (lines_list):
 	return new_lines
 
 
+def avg_close_lines_2 (lines_list):
+	"""
+		Function: avg_close_points 
+		--------------------------
+		given a list of keypoints, this returns another list of 
+		(x, y) pairs for points that are very close 
+	"""
+	lines = [(rho, theta) for rho, theta in lines_list]
+
+	#=====[ Step 1: get points out of each one	]=====
+	old_lines = np.array(lines)
+
+	#=====[ Step 2: get new_points	]=====
+	new_lines = []
+	while len(old_lines	) > 1:
+		l1 = old_lines [0]
+		distances = np.array([abs(l1[0] - l2[0]) for l2 in old_lines])
+		idx = (distances < 10)
+		lines_cluster = old_lines[idx]
+		new_line = point_avg (lines_cluster)
+		new_lines.append (new_line)
+		old_lines = old_lines[np.invert(idx)]
+
+	return new_lines
+
+
+
 def rho_theta_to_abc (line):
 	"""
 		Function: rho_theta_to_abc
@@ -86,7 +113,18 @@ def filter_by_slope (line_list):
 		picks
 	"""
 	abc_lines = [rho_theta_to_abc(l) for l in line_list]
-	return [l_rt for l_rt, l_abc in zip(line_list, abc_lines) if abs(l_abc[0]/l_abc[1]) > 1]
+	return [l_rt for l_rt, l_abc in zip(line_list, abc_lines) if abs(l_abc[0]/(l_abc[1] + 0.0000967)) > 1]
+
+def filter_by_slope_2 (line_list):
+	"""
+		Function: filter_by_slope
+		-------------------------
+		given a list of lines in rho, theta form,
+		picks
+	"""
+	abc_lines = [rho_theta_to_abc(l) for l in line_list]
+	return np.array([l_rt for l_rt, l_abc in zip(line_list, abc_lines) if abs(l_abc[0]/(l_abc[1] + 0.000096)) < 0.1])
+
 
 
 def get_line_point_distance (line, point):
@@ -155,12 +193,21 @@ if __name__ == '__main__':
 
 	#=====[ Step 5: snap points to grid	]=====
 	points_grid = get_points_grid (lines, corners)
-	print len(points_grid)
 	for i in range(len(points_grid)):
 		for point in points_grid[i]:
 			p = (int(point[0]), int(point[1]))
 			cv2.circle (image, p, 5, (0, 0, 255), -1)
 
+
+	#=====[ Step 6: hough transform on remaining points	]=====
+	all_points = [p for l in points_grid for p in l]
+	corners_img = np.zeros (image.shape[:2], dtype=np.uint8)
+	for p in all_points:
+		corners_img[int(p[1])][int(p[0])] = 255
+	lines = cv2.HoughLines (corners_img, 3, np.pi/180, 2)
+	lines = lines [0]
+	lines = filter_by_slope_2 (lines)
+	lines = avg_close_lines_2 (lines)
 
 
 	# #####[ VISUALIZE CORNERS	]#####
@@ -179,6 +226,6 @@ if __name__ == '__main__':
 		y1 = int(y0 + 1000*(a))    # But if you want to round the number, then use np.around() function, then 3.8 --> 4.0
 		x2 = int(x0 - 1000*(-b))   # But we need integers, so use int() function after that, ie int(np.around(x))
 		y2 = int(y0 - 1000*(a))
-		cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
+		cv2.line(image,(x1,y1),(x2,y2),(255,0,0),2)
 	cv2.namedWindow ('houghLines')
 	cv2.imshow ('houghLines', image)
